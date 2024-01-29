c68aebed9d759660ee2d7dc5d06a8b38cc60d90b
Ju Chen <chenju816@gmail.com>
2024-01-29 11:39:02 +0800

    barrage effect implemented

--- entry/src/main/ets/pages/barrageExample.js
+++ entry/src/main/ets/pages/barrageExample.js
@@ -1,6 +1,15 @@
 "use strict";
 struct BarrageExample extends   {
     constructor() { }
+    startScroll(start, end) {
+        this.timer = setInterval(() => {
+            this.xOffset = (this.xOffset * 10 + 4) / 10;
+            this.scroller.scrollTo({ x: this.xOffset, y: 0 });
+        }, 1000 / 60);
+    }
+    stopScroll() {
+        clearInterval(this.timer);
+    }
     build() {
         .width('100%');
     }
